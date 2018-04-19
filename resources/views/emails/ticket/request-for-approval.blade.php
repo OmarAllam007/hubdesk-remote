0843658f23e2b3fa09e26ca15dfdcfe6b5c07bac
Omar Khaled <omar.ceh007@gmail.com>
2018-04-19 09:39:14 +0300

    approval template

--- resources/views/emails/ticket/request-for-approval.blade.php
+++ resources/views/emails/ticket/request-for-approval.blade.php
@@ -13,10 +13,15 @@
 
     <br><br>
 </div>
-<div style="padding-left: 10px;">
+<div style="padding-left: 15px;">
     @component('mail::button', ['url' => route('approval.show',$approval->id)])
         <b class="center-block">Click here to take action</b>
     @endcomponent
+        <div class="alert alert-info" role="alert">
+            <p  style="background-color:#f6f7d2; border-radius: 5px;font-size: small; padding: 10px;margin: 10px;text-align: center">
+                {{t('Please don\'t reply on this email and give the approval through the system')}}
+            </p>
+        </div>
 </div>
 
 
