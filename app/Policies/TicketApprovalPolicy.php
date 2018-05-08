505833d403fdce9e0014bcea1cb81840a4728f0b
omar <omar.coder007@gmail.com>
2018-05-08 09:57:02 +0300

    report authorization

--- app/Policies/TicketApprovalPolicy.php
+++ app/Policies/TicketApprovalPolicy.php
@@ -10,50 +10,9 @@ class TicketApprovalPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the ticketApproval.
-     *
-     * @param  \App\User  $user
-     * @param  \App\TicketApproval  $ticketApproval
-     * @return mixed
-     */
-    public function view(User $user, TicketApproval $ticketApproval)
-    {
-        //
-    }
 
-    /**
-     * Determine whether the user can create ticketApprovals.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the ticketApproval.
-     *
-     * @param  \App\User  $user
-     * @param  \App\TicketApproval  $ticketApproval
-     * @return mixed
-     */
-    public function update(User $user, TicketApproval $ticketApproval)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the ticketApproval.
-     *
-     * @param  \App\User  $user
-     * @param  \App\TicketApproval  $ticketApproval
-     * @return mixed
-     */
     public function delete(User $user, TicketApproval $ticketApproval)
     {
-       return $user->id == $ticketApproval->creator_id || $user->id == ($ticketApproval->ticket->technician && $ticketApproval->ticket->technician->id);
+       return $user->id == $ticketApproval->creator_id || ($ticketApproval->ticket->technician && $user->id == $ticketApproval->ticket->technician->id);
     }
 }
