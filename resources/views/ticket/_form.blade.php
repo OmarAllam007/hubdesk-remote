2e5039d5bc37567f63dcee2dd023ef1c295dae29
omarkhaled <omar.ceh007@gmail.com>
2019-02-28 14:44:48 +0300

    change-ticket-create-design

--- resources/views/ticket/_form.blade.php
+++ resources/views/ticket/_form.blade.php
@@ -1,20 +1,25 @@
 {{ csrf_field() }}
 <div id="TicketForm">
-    @if (!isset($ticket) && Auth::user()->isSupport())
-        <div class="form-group form-group-sm {{$errors->has('requester_id')? 'has-error' : ''}}">
-            {{ Form::label('requester_id', t('Requester'), ['class' => 'control-label']) }}
-            {{ Form::select('requester_id', App\User::requesterList()->prepend('Create for me', ''), null, ['class' => 'form-control select2']) }}
-            {!! $errors->first('requester_id', '<div class="error-message">:message</div>') !!}
-        </div>
-    @endif
+    <div class="row">
+        <div class="col-sm-6">
+            @if (!isset($ticket) && Auth::user()->isSupport())
+                <div class="form-group form-group-sm {{$errors->has('requester_id')? 'has-error' : ''}}">
+                    {{ Form::label('requester_id', t('Requester'), ['class' => 'control-label']) }}
+                    {{ Form::select('requester_id', App\User::requesterList()->prepend('Create for me', ''), null, ['class' => 'form-control select2']) }}
+                    {!! $errors->first('requester_id', '<div class="error-message">:message</div>') !!}
+                </div>
+            @endif
 
-    <div class="form-group form-group-sm {{$errors->has('subject')? 'has-error' : ''}}">
-        {{ Form::label('subject', t('Subject'), ['class' => 'control-label']) }}
-        {{ Form::text('subject', null, ['class' => 'form-control']) }}
-        @if ($errors->has('subject'))
-            <div class="error-message">{{$errors->first('subject')}}</div>
-        @endif
+            <div class="form-group form-group-sm {{$errors->has('subject')? 'has-error' : ''}}">
+                {{ Form::label('subject', t('Subject'), ['class' => 'control-label']) }}
+                {{ Form::text('subject', $category->name.'  -  '.$subcategory->name ?? '', ['class' => 'form-control']) }}
+                @if ($errors->has('subject'))
+                    <div class="error-message">{{$errors->first('subject')}}</div>
+                @endif
+            </div>
+        </div>
     </div>
+
 
     <div class="row">
         <div class="col-sm-7">
@@ -26,11 +31,6 @@
                 @endif
             </div>
 
-            <div class="row">
-                <div class="col-md-9">
-                    <attachments limit="5"></attachments>
-                </div>
-            </div>
         </div>
         <div class="col-sm-5">
             <div class="form-group form-group-sm {{$errors->has('category_id')? 'has-error' : ''}}">
@@ -38,15 +38,17 @@
                 {{ Form::select('category_id', App\Category::selection('Select Category'), request('category_id'), ['class' => 'form-control', 'v-model' => 'category','disabled'=>'disabled']) }}
                 @if ($errors->has('category_id'))
                     <div class="error-message">{{$errors->first('category_id')}}</div>
+                @endif
             </div>
-            @endif
 
             <div class="form-group form-group-sm {{$errors->has('subcategory')? 'has-error' : ''}}">
                 {{ Form::label('subcategory_id', t('Subcategory'), ['class' => 'control-label']) }}
 
-                <select class="form-control" name="subcategory_id" id="subcategory_id" v-model="subcategory" disabled="disabled">
+                <select class="form-control" name="subcategory_id" id="subcategory_id" v-model="subcategory"
+                        disabled="disabled">
                     <option value="">Select Subcategory</option>
-                    <option v-for="(subcategory, id) in subcategories" :value="subcategory.id">@{{subcategory.name}}</option>
+                    <option v-for="(subcategory, id) in subcategories" :value="subcategory.id">@{{subcategory.name}}
+                    </option>
                 </select>
                 @if ($errors->has('subcategory_id'))
                     <div class="error-message">{{$errors->first('subcategory_id')}}</div>
@@ -64,6 +66,11 @@
                 @endif
             </div>
 
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-4">
             <div id="CustomFields">
                 @include('custom-fields.render', [
                     'category' => App\Category::find(old('category_id')),
@@ -75,53 +82,59 @@
     </div>
 
 
+    <div class="row">
+        <div class="col-md-6">
+            <attachments limit="5"></attachments>
+        </div>
+    </div>
+
     <hr class="form-divider">
 
-        {{--<div class="col-sm-6">
-            <div class="form-group form-group-sm {{$errors->has('impact_id')? 'has-error' : ''}}">
-                {{ Form::label('impact_id', 'Impact', ['class' => 'control-label']) }}
-                {{ Form::select('impact_id', App\Impact::selection('Select Impact'), null, ['class' => 'form-control']) }}
-                @if ($errors->has('impact_id'))
-                    <div class="error-message">{{$errors->first('impact_id')}}</div>
-                @endif
-            </div>
-            <div class="form-group form-group-sm {{$errors->has('priority_id')? 'has-error' : ''}}">
-                {{ Form::label('priority_id', 'Priority', ['class' => 'control-label']) }}
-                {{ Form::select('priority_id', App\Priority::selection('Select Priority'), null, ['class' => 'form-control']) }}
-                @if ($errors->has('priority_id'))
-                    <div class="error-message">{{$errors->first('priority_id')}}</div>
-                @endif
-            </div>
-        </div>--}}
+    {{--<div class="col-sm-6">
+        <div class="form-group form-group-sm {{$errors->has('impact_id')? 'has-error' : ''}}">
+            {{ Form::label('impact_id', 'Impact', ['class' => 'control-label']) }}
+            {{ Form::select('impact_id', App\Impact::selection('Select Impact'), null, ['class' => 'form-control']) }}
+            @if ($errors->has('impact_id'))
+                <div class="error-message">{{$errors->first('impact_id')}}</div>
+            @endif
+        </div>
+        <div class="form-group form-group-sm {{$errors->has('priority_id')? 'has-error' : ''}}">
+            {{ Form::label('priority_id', 'Priority', ['class' => 'control-label']) }}
+            {{ Form::select('priority_id', App\Priority::selection('Select Priority'), null, ['class' => 'form-control']) }}
+            @if ($errors->has('priority_id'))
+                <div class="error-message">{{$errors->first('priority_id')}}</div>
+            @endif
+        </div>
+    </div>--}}
 
 
-        {{--@if (Auth::user()->isSupport())--}}
-            {{--<div class="col-sm-6">--}}
-                {{--<div class="form-group form-group-sm {{$errors->has('group_id')? 'has-error' : ''}}">--}}
-                    {{--{{ Form::label('group_id', t('Group'), ['class' => 'control-label']) }}--}}
-                    {{--{{ Form::select('group_id', App\Group::support()->selection('Select Group'), null, ['class' => 'form-control']) }}--}}
-                    {{--@if ($errors->has('group_id'))--}}
-                        {{--<div class="error-message">{{$errors->first('group')}}</div>--}}
-                    {{--@endif--}}
-                {{--</div>--}}
+    {{--@if (Auth::user()->isSupport())--}}
+    {{--<div class="col-sm-6">--}}
+    {{--<div class="form-group form-group-sm {{$errors->has('group_id')? 'has-error' : ''}}">--}}
+    {{--{{ Form::label('group_id', t('Group'), ['class' => 'control-label']) }}--}}
+    {{--{{ Form::select('group_id', App\Group::support()->selection('Select Group'), null, ['class' => 'form-control']) }}--}}
+    {{--@if ($errors->has('group_id'))--}}
+    {{--<div class="error-message">{{$errors->first('group')}}</div>--}}
+    {{--@endif--}}
+    {{--</div>--}}
 
-                {{--<div class="form-group form-group-sm {{$errors->has('technician_id')? 'has-error' : ''}}">--}}
-                    {{--{{ Form::label('technician_id', t('Technician'), ['class' => 'control-label']) }}--}}
-                    {{--{{ Form::select('technician_id', App\User::technicians()->selection('Select Technician'), null, ['class' => 'form-control']) }}--}}
-                    {{--@if ($errors->has('technician_id'))--}}
-                        {{--<div class="error-message">{{$errors->first('technician_id')}}</div>--}}
-                    {{--@endif--}}
-                {{--</div>--}}
+    {{--<div class="form-group form-group-sm {{$errors->has('technician_id')? 'has-error' : ''}}">--}}
+    {{--{{ Form::label('technician_id', t('Technician'), ['class' => 'control-label']) }}--}}
+    {{--{{ Form::select('technician_id', App\User::technicians()->selection('Select Technician'), null, ['class' => 'form-control']) }}--}}
+    {{--@if ($errors->has('technician_id'))--}}
+    {{--<div class="error-message">{{$errors->first('technician_id')}}</div>--}}
+    {{--@endif--}}
+    {{--</div>--}}
 
-                {{--<div class="form-group form-group-sm {{$errors->has('urgency_id')? 'has-error' : ''}}">--}}
-                    {{--{{ Form::label('urgency_id', t('Urgency'), ['class' => 'control-label']) }}--}}
-                    {{--{{ Form::select('urgency_id', App\Urgency::selection('Select Urgency'), null, ['class' => 'form-control']) }}--}}
-                    {{--@if ($errors->has('urgency_id'))--}}
-                        {{--<div class="error-message">{{$errors->first('urgency_id')}}</div>--}}
-                    {{--@endif--}}
-                {{--</div>--}}
-            {{--</div>--}}
-        {{--@endif--}}
+    {{--<div class="form-group form-group-sm {{$errors->has('urgency_id')? 'has-error' : ''}}">--}}
+    {{--{{ Form::label('urgency_id', t('Urgency'), ['class' => 'control-label']) }}--}}
+    {{--{{ Form::select('urgency_id', App\Urgency::selection('Select Urgency'), null, ['class' => 'form-control']) }}--}}
+    {{--@if ($errors->has('urgency_id'))--}}
+    {{--<div class="error-message">{{$errors->first('urgency_id')}}</div>--}}
+    {{--@endif--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--@endif--}}
 
 </div>
 <div class="row">
