c3731e371c27895e2bebd133b2c7fd04be3cc6a2
omarkhaled <omar.ceh007@gmail.com>
2019-02-28 13:30:27 +0300

    fix-autoselect-category

--- resources/views/ticket/_form.blade.php
+++ resources/views/ticket/_form.blade.php
@@ -131,12 +131,3 @@
         </div>
     </div>
 </div>
-@section('javascript')
-    <script>
-        var category = '{{Form::getValueAttribute('category_id') ? Form::getValueAttribute('category_id') : isset($category) ? $category->id : null}}';
-        var subcategory = '{{request('subcategory_id') ? Form::getValueAttribute('subcategory_id', request('subcategory_id')) :  isset($subcategory) ? $subcategory->id : null}}';
-        var item = '{{request('item_id') ? Form::getValueAttribute('item_id',request('item_id')) :  isset($item) ? $item->id : null}}';
-    </script>
-    <script src="{{asset('/js/ticket-form.js')}}"></script>
-    <script src="{{asset('/js/tinymce/tinymce.min.js')}}"></script>
-@append
