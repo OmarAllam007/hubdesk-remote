cff74a35253f1108eac0fa0df26d1364b8d3258d
omarkhaled <omar.ceh007@gmail.com>
2019-03-04 13:35:50 +0300

    some changes to be continued

--- resources/views/ticket/_form.blade.php
+++ resources/views/ticket/_form.blade.php
@@ -2,13 +2,15 @@
 <div id="TicketForm">
     <div class="row">
         <div class="col-md-6">
+            @if($errors->count())
                 <div class="alert alert-danger">
                     <ul>
                         @foreach($errors->all() as $error)
-                        <li>{{$error}}</li>
+                            <li>{{$error}}</li>
                         @endforeach
                     </ul>
                 </div>
+            @endif
         </div>
     </div>
     <div class="row">
