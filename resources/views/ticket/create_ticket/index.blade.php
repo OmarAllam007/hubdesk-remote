8c43aaf2dbbebd11817e888f22df13f8b289e55d
omar <omar.coder007@gmail.com>
2019-04-14 21:12:50 +0300

    before presentation

--- resources/views/ticket/create_ticket/index.blade.php
+++ resources/views/ticket/create_ticket/index.blade.php
@@ -13,9 +13,26 @@
 @section('body')
     <section class="col-md-12 ">
         <div class=form-group></div>
-        <h3 class=text-center>{{t('Select Business Unit') }}</h3>
+{{--        <h3 class=text-center>{{t('Select Business Unit') }}</h3>--}}
 
         <div class="tiles-container">
+            <a class="tile" href="https://fiori.alkifah.com:5447/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html?sap-client=900&sap-language=EN&sap-sec_session_created=X" target="_blank">
+                <div class="tile-container" style="display: flex; flex-direction:column;align-items: center;justify-content: center;">
+                    {{--<div class="tile-icon" style="">--}}
+                    {{--                            <img src="{{asset('images/logo.png')}}">--}}
+                    {{--</div>--}}
+                    <div class="tile-body" style="width: 100%;height: 100%;display: flex; flex-direction:column;">
+                        {{--@if($business_unit->logo)--}}
+                        <p class="text-center" >
+                            <img src="{{asset('images/f-logo.jpg')}}" >
+                        </p>
+                        {{--@endif--}}
+                        <p class="text-center" style="margin-top: 40px">
+{{--                            {{$business_unit->name}}--}}
+                        </p>
+                    </div>
+                </div>
+            </a>
             @foreach(\App\BusinessUnit::whereHas('categories')->orderBy('name')->get() as $business_unit)
                 <a href="{{route('ticket.create.select_category', $business_unit)}}" class="tile">
                     <div class="tile-container" style="display: flex; flex-direction:column;align-items: center;justify-content: center;">
