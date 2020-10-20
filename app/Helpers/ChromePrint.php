c55d53f44655225f21fcacb01e6ee8fb325ec47c
omarkhaled <omar.ceh007@gmail.com>
2020-10-20 07:08:55 +0300

    wip

--- app/Helpers/ChromePrint.php
+++ app/Helpers/ChromePrint.php
@@ -1,16 +1,17 @@
 <?php
 
-
 namespace App\Helpers;
+
 use ChromeDevtoolsProtocol\Context;
 use ChromeDevtoolsProtocol\Instance\Launcher;
 use ChromeDevtoolsProtocol\Model\Page\NavigateRequest;
 use ChromeDevtoolsProtocol\Model\Page\PrintToPDFRequest;
 use Exception;
+use Spatie\Browsershot\Browsershot;
 
 class ChromePrint
 {
-    public $file;
+    private $file;
 
     public function __construct($file)
     {
@@ -23,41 +24,19 @@ class ChromePrint
         // We use retry here to repeat the operation if the exception is thrown to ensure generating the file
 
         return retry(3, function () {
-            $ctx = Context::withTimeout(Context::background(), 1800);
-            $launcher = new Launcher();
-            $launcher->setExecutable(config('app.chrome_path'));
-            $instance = $launcher->launch($ctx);
+            $url = $this->file;
+            if (substr($this->file, 0, 4) !== 'http') {
+                $url = "file://$this->file";
+            }
+            $file = storage_path('app/' . uniqid('print_request_') . '.pdf');
 
-            $tab = $instance->open($ctx);
-            $tab->activate($ctx);
-            $devtools = $tab->devtools();
-
-            $page = $devtools->page();
-//            $options = [
-//                'landscape'       => true,  // default to false
-//                'printBackground' => true,   // default to false
-//                'displayHeaderFooter' => true, // default to false
-//                'preferCSSPageSize' => true, // default to false ( reads parameters directly from @page )
-//                'marginTop' => 0.0, // defaults to ~0.4 (must be float, value in inches)
-//                'marginBottom' => 1.4, // defaults to ~0.4 (must be float, value in inches)
-//                'marginLeft' => 5.0, // defaults to ~0.4 (must be float, value in inches)
-//                'marginRight' => 1.0 // defaults to ~0.4 (must be float, value in inches)
-//               ];
-
-
-            $page->enable($ctx);
-            $page->navigate($ctx, NavigateRequest::builder()->setUrl("file://$this->file")->build());
-            $page->awaitLoadEventFired($ctx);
-            $request = PrintToPDFRequest::builder()->setDisplayHeaderFooter(false)
-                ->setPrintBackground(true)
-                ->setPreferCSSPageSize(true)
-                ->build();
-
-            $file = storage_path('app/' . uniqid('print_report_') . '.pdf');
-            file_put_contents($file, base64_decode($page->printToPDF($ctx, $request)->data));
-
-            $devtools->close();
-            $instance->close();
+            Browsershot::url($url)
+                ->waitUntilNetworkIdle()
+                ->showBackground()
+                ->windowSize(1280, 1024)
+                ->timeout(300)
+                ->margins(0.5, 0.5, 0.5, 0.5, 'in')
+                ->format('A4')->savePdf($file);
 
             if ($file && file_exists($file)) {
                 return $file;
