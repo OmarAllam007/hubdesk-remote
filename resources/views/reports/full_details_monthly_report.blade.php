516d8b44dee1d0d03b3cb07c83d3bdb4522cf1f0
omarkhaled <omar.ceh007@gmail.com>
2020-11-12 14:31:59 +0300

    full detailed report

--- resources/views/reports/full_details_monthly_report.blade.php
+++ resources/views/reports/full_details_monthly_report.blade.php
@@ -25,44 +25,43 @@
 @endsection
 
 @section('body')
-    <div class="container  col-md-12">
-        <section class="report-horizontal-scroll search-table-outter">
-            <table class=" search-table inner">
-                <thead style="background-color: #194F7E;color: white">
+    <div class="overflow-x-auto bg-white rounded-lg shadow relative h-full m-5">
+        <table class="border-collapse table-auto w-full whitespace-no-wrap  relative">
+            <thead>
                 <tr>
-                    <th colspan="11" class="text-center">Ticket Details</th>
-                    <th colspan="{{($fields_count*2)-2}}" class="text-center">Ticket Additional Fields</th>
-                    <th colspan="{{($replies_count*2)-2}}" class="text-center">Ticket Replies</th>
-                    <th colspan="{{($approvals_count*3)-3}}" class="text-center">Ticket Approvals</th>
+                    <th colspan="11" class="text-center py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">Ticket Details</th>
+                    <th colspan="{{($fields_count*2)-2}}" class="text-center py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">Ticket Additional Fields</th>
+                    <th colspan="{{($replies_count*2)-2}}" class="text-center py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">Ticket Replies</th>
+                    <th colspan="{{($approvals_count*3)-3}}" class="text-center py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">Ticket Approvals</th>
                 </tr>
                 <tr>
-                    <th>{{ t('Request ID') }}</th>
-                    <th>{{ t('Subject') }}</th>
-                    <th>{{ t('Requester') }}</th>
-                    <th>{{ t('Technician') }}</th>
-                    <th>{{ t('Category') }}</th>
-                    <th>{{ t('Subcategory') }}</th>
-                    <th>{{ t('Status') }}</th>
-                    <th>{{ t('Created Date') }}</th>
-                    <th>{{ t('Due Date') }}</th>
-                    <th>{{ t('Resolved Date') }}</th>
-                    <th>{{ t('Business Unit') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Request ID') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Subject') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Requester') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Technician') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Category') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Subcategory') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Status') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Created Date') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Due Date') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Resolved Date') }}</th>
+                    <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{ t('Business Unit') }}</th>
 
 
                     @for ($i = 1; $i < $fields_count; $i++)
-                        <th>{{t('Field Name')}}</th>
-                        <th>{{t('Field Value')}}</th>
+                        <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{t('Field Name')}}</th>
+                        <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{t('Field Value')}}</th>
                     @endfor
 
                     @for ($i = 1; $i < $replies_count; $i++)
-                        <th>{{t('Reply Content')}}</th>
-                        <th>{{t('Reply Status')}}</th>
+                        <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{t('Reply Content')}}</th>
+                        <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{t('Reply Status')}}</th>
                     @endfor
 
                     @for ($i = 1; $i < $approvals_count; $i++)
-                        <th>{{t('Approval '.$i.' Sent at')}}</th>
-                        <th>{{t('Approval '.$i.' Action date')}}</th>
-                        <th>{{t('Approval '.$i.' Status')}}</th>
+                        <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{t('Approval '.$i.' Sent at')}}</th>
+                        <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{t('Approval '.$i.' Action date')}}</th>
+                        <th class="py-3 px-6 sticky top-0 border-b border border-gray-600 bg-blue-600 text-white">{{t('Approval '.$i.' Status')}}</th>
                     @endfor
 
 
@@ -70,44 +69,39 @@
                 </thead>
                 <tbody>
                 @foreach ($data as $ticket)
-                    <tr>
-                        <td>{{ $ticket->id }}</td>
-                        <td>{{ $ticket->subject }}</td>
-                        <td>{{ $ticket->requester ?? 'Not Assigned' }}</td>
-                        <td>{{ $ticket->technician ?? 'Not Assigned' }}</td>
-                        <td>{{ $ticket->category ?? 'Not Assigned' }}</td>
-                        <td>{{ $ticket->subcategory ?? 'Not Assigned' }}</td>
-                        <td>{{ $ticket->status ?? 'Not Assigned' }}</td>
-                        <td>{{ $ticket->created_at ?? 'Not Assigned' }}</td>
-                        <td>{{ $ticket->due_date ?? 'Not Assigned' }}</td>
-                        <td>{{ $ticket->resolve_date ?? 'Not Assigned' }}</td>
-                        <td>{{ $ticket->business_unit ?? 'Not Assigned' }}</td>
+                    <tr class="hover:bg-yellow-200">
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->id }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->subject }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->requester ?? 'Not Assigned' }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->technician ?? 'Not Assigned' }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->category ?? 'Not Assigned' }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->subcategory ?? 'Not Assigned' }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->status ?? 'Not Assigned' }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->created_at ?? 'Not Assigned' }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->due_date ?? 'Not Assigned' }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->resolve_date ? $ticket->resolve_date : ($ticket->close_date ? $ticket->close_date : 'Not Assigned')  }}</td>
+                        <td class="py-4 px-6  border border-gray-600 ">{{ $ticket->business_unit ?? 'Not Assigned' }}</td>
 
 
                         @for ($i = 1; $i < $fields_count; $i++)
-                            <td>{{ isset($ticket->fields[$i]) ? $ticket->fields[$i]->name : '' }}</td>
-                            <td>{{ isset($ticket->fields[$i]) ? $ticket->fields[$i]->value : '' }}</td>
+                            <td class="py-4 px-6  border border-gray-600 ">{{ isset($ticket->fields[$i]) ? $ticket->fields[$i]->name : '' }}</td>
+                            <td class="py-4 px-6  border border-gray-600 ">{{ isset($ticket->fields[$i]) ? $ticket->fields[$i]->value : '' }}</td>
                         @endfor
 
                         @for ($i = 1; $i < $replies_count; $i++)
-                            <td>{{ isset($ticket->replies[$i]) ? strip_tags($ticket->replies[$i]->content) : '' }}</td>
-                            <td>{{ isset($ticket->replies[$i]) ? $ticket->replies[$i]->status->name : '' }}</td>
+                            <td class="py-4 px-6  border border-gray-600 ">{{ isset($ticket->replies[$i]) ? strip_tags($ticket->replies[$i]->content) : '' }}</td>
+                            <td class="py-4 px-6  border border-gray-600 ">{{ isset($ticket->replies[$i]) ? $ticket->replies[$i]->status->name : '' }}</td>
                         @endfor
 
                         @for ($i = 1; $i < $approvals_count; $i++)
-                            <td>{{ isset($ticket->approvals[$i]) ? $ticket->approvals[$i]->created_at->format('Y/m/d h:i') : '' }}</td>
-                            <td>{{ isset($ticket->approvals[$i]) && $ticket->approvals[$i]->approval_date ? $ticket->approvals[$i]->approval_date->format('Y/m/d h:i') : '' }}</td>
-                            <td>{{ isset($ticket->approvals[$i]) ? App\TicketApproval::$statuses[$ticket->approvals[$i]->status] : '' }}</td>
+                            <td class="py-4 px-6  border border-gray-600 ">{{ isset($ticket->approvals[$i]) ? $ticket->approvals[$i]->created_at->format('Y/m/d h:i') : '' }}</td>
+                            <td class="py-4 px-6  border border-gray-600 ">{{ isset($ticket->approvals[$i]) && $ticket->approvals[$i]->approval_date ? $ticket->approvals[$i]->approval_date->format('Y/m/d h:i') : '' }}</td>
+                            <td class="py-4 px-6  border border-gray-600 ">{{ isset($ticket->approvals[$i]) ? App\TicketApproval::$statuses[$ticket->approvals[$i]->status] : '' }}</td>
                         @endfor
-
-
-
-
                     </tr>
                 @endforeach
                 </tbody>
             </table>
-        </section>
     </div>
     <div class="text-center">
         {{ $data->links() }}
