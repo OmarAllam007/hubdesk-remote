5912025f93d687c59aff8554af3da3a4b6e1849e
omarkhaled <omar.ceh007@gmail.com>
2020-11-18 14:53:19 +0300

    add reset password to hubdesk

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -6,7 +6,7 @@
 @if ($level == 'error')
 # Whoops!
 @else
-# Hello!
+# {{ $actionText }}
 @endif
 @endif
 
@@ -19,16 +19,16 @@
 {{-- Action Button --}}
 @isset($actionText)
 <?php
-    switch ($level) {
-        case 'success':
-            $color = 'green';
-            break;
-        case 'error':
-            $color = 'red';
-            break;
-        default:
-            $color = 'blue';
-    }
+switch ($level) {
+case 'success':
+$color = 'green';
+break;
+case 'error':
+$color = 'red';
+break;
+default:
+$color = 'blue';
+}
 ?>
 @component('mail::button', ['url' => $actionUrl, 'color' => $color])
 {{ $actionText }}
@@ -48,11 +48,10 @@
 Regards,<br>{{ config('app.name') }}
 @endif
 
-{{-- Subcopy --}}
-@isset($actionText)
-@component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
-@endcomponent
-@endisset
+{{--@isset($actionText)--}}
+{{--@component('mail::subcopy')--}}
+{{--If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below--}}
+{{--into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})--}}
+{{--@endcomponent--}}
+{{--@endisset--}}
 @endcomponent
