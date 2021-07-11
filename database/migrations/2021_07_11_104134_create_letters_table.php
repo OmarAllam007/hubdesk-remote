a36c3fe92aedc3819f7ca0eef02ae3d924c4ebe3
Omar Khaled <omarkhaled@OmarGarana.local>
2021-07-11 11:02:55 +0300

    wi

---
+++ database/migrations/2021_07_11_104134_create_letters_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLettersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('letters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('letter_group_id');
+            $table->unsignedInteger('order')->nullable()->default(0);
+            $table->string('view_path')->nullable();
+            $table->string('icon_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('letters');
+    }
+}
