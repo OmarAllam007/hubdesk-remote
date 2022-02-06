784d6702582cb65ef5149626777d4636b4824054
omarkhaled <omar.ceh007@gmail.com>
2022-02-06 15:28:09 +0300

    add qr to letter ticket

--- resources/views/letters/verification/index.blade.php
+++ resources/views/letters/verification/index.blade.php
@@ -6,11 +6,14 @@
 <body>
 @section('body')
     <div class="flex flex-col ">
-
         <div class="flex justify-center">
             <div class="logo"><a href="{{url('/')}}"><img src="{{asset('images/holding_logo.png')}}"></a></div>
+        </div>
+        <div class="flex justify-center">
+
             <iframe id="salarySlip"
-                    src="{{route('letters.generate.pdf',$ticketID)}}" scrolling="auto" height="1100" class="w-2/3 bg-white"></iframe>
+                    src="{{route('letters.generate.pdf',$ticketID)}}" scrolling="auto" height="1100"
+                    class="w-2/3 bg-white"></iframe>
         </div>
     </div>
 
