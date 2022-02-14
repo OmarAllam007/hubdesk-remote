1bf7829d160e0faa8f062e4cc42967a340b76a23
omarkhaled <omar.ceh007@gmail.com>
2022-02-14 11:24:40 +0300

    sabb bank

--- resources/views/letters/template/salary_transfer/sabb.blade.php
+++ resources/views/letters/template/salary_transfer/sabb.blade.php
@@ -20,9 +20,9 @@
             margin: 0 auto;
             background-repeat: no-repeat;
             @if(\App\LetterTicket::isApprovedTicket($letterTicket->ticket))
-                                           background-image: url({{url("/storage/headers/{$letterTicket->header}/image.jpg")}}) !important;
+                                              background-image: url({{url("/storage/headers/{$letterTicket->header}/image.jpg")}}) !important;
             @endif
-                                           background-size: contain !important;
+                                              background-size: contain !important;
         }
 
 
@@ -42,7 +42,6 @@
             <div class="pt-32"></div>
             <div class="pt-10 "></div>
             <div class="flex justify-end pt-20 px-10 ">
-
                 <p class="text-gray-900 text-xl  pt-10"> الموافق : {{$letterTicket->last_approval_date}}
                     م</p>
             </div>
@@ -50,7 +49,6 @@
                 <p class="text-2xl  ">
                     @php
                         $IbanField = $letterTicket->ticket->fields->first() ? $letterTicket->ticket->fields->first()->value : '';
-
                     @endphp
                     الســادة / {{$letterTicket->letter->ar_name}}
                 </p>
@@ -104,7 +102,6 @@
                         <span>{{$user['occupation']}}</span>
                     </p>
                 </div>
-
                 <div class="w-1/2">
                     <p class="text-2xl  " dir="rtl">
                         <span>القسم:</span>
@@ -121,26 +118,27 @@
                     </p>
                 </div>
 
-                <div class="w-1/2">
-                    <p class="text-2xl  " dir="rtl">
-                        <span>مستحقات نهاية الخدمة :</span>
-                        <span>{{$user['eos_amount']}}</span>
-                    </p>
-                </div>
+                {{--                <div class="w-1/2">--}}
+                {{--                    <p class="text-2xl  " dir="rtl">--}}
+                {{--                        <span>مستحقات نهاية الخدمة :</span>--}}
+                {{--                        <span>{{$user['eos_amount']}}</span>--}}
+                {{--                    </p>--}}
+                {{--                </div>--}}
             </div>
 
             <div class="flex  pt-5 px-10 " dir="rtl">
                 <div class="w-1/2">
                     <p class="text-2xl  " dir="rtl">
                         <span>وضع الوظيفه:</span>
-                        <span>{{$user['job_status']}}</span>
+                        <span>مثبت</span>
                     </p>
                 </div>
 
                 <div class="w-1/2">
                     <p class="text-2xl  " dir="rtl">
                         <span>عقد العمل:</span>
-                        <span>{{$user['work_contract']}}</span>
+                        {{--                        <span>{{$user['work_contract']}}</span>--}}
+                        <span>يجدد تلقائيا</span>
                     </p>
                 </div>
             </div>
@@ -149,14 +147,16 @@
                 <div class="w-1/2">
                     <p class="text-2xl  " dir="rtl">
                         <span>الراتب الاساسي:</span>
-                        <span>{{$user['allowances']['basic_salary']}}</span>
+                        <span>{{$user['allowances']['basic_salary']}} ريال</span>
                     </p>
                 </div>
 
                 <div class="w-1/2">
                     <p class="text-2xl  " dir="rtl">
                         <span>الحسميات:</span>
-                        <span>{{$user['discounts']}}</span>
+                        <span> لايوجد
+{{--                            {{$user['discounts']}}--}}
+                        </span>
                     </p>
                 </div>
             </div>
@@ -165,7 +165,7 @@
                 <div class="w-1/2">
                     <p class="text-2xl  " dir="rtl">
                         <span>بدلات:</span>
-                        <span>  بدل السكن: {{$user['allowances']['housing_allowance']}}</span>
+                        <span>  بدل السكن: {{$user['allowances']['housing_allowance']}} ريال</span>
                     </p>
                 </div>
 
@@ -173,7 +173,7 @@
                     <div class="w-1/2">
                         <p class="text-2xl  " dir="rtl">
                             <span>بدل نقل:</span>
-                            <span>{{$user['allowances']['transportation_allowance']}}</span>
+                            <span>{{$user['allowances']['transportation_allowance']}} ريال</span>
                         </p>
                     </div>
                 @endif
@@ -186,19 +186,45 @@
                         <div class="w-1/2">
                             <p class="text-2xl  " dir="rtl">
                                 <span>بدل طبيعة عمل :</span>
-                                <span>  {{$user['allowances']['nature_of_work_allowance']}}</span>
+                                <span>  {{$user['allowances']['nature_of_work_allowance']}} ريال</span>
                             </p>
                         </div>
                     @endif
-                    @if(isset($user['allowances']['food_allowance']))
 
+                    @if(isset($user['allowances']['transportation_allowance']))
                         <div class="w-1/2">
                             <p class="text-2xl  " dir="rtl">
-                                <span>بدل طعام:</span>
-                                <span>{{$user['allowances']['food_allowance']}}</span>
-                            </p>
-                        </div>
+                                بدل نقل ( {{$user['allowances']['transportation_allowance']}} )
+                            </p></div>
                     @endif
+                    @if(isset($user['allowances']['nature_of_work_allowance']))
+                        <div class="w-1/2">
+                            <p class="text-2xl  " dir="rtl">
+                                بدل طبيعة عمل ( {{$user['allowances']['nature_of_work_allowance']}} )
+                            </p></div>
+                    @endif
+
+                    @if(isset($user['allowances']['food_allowance']))
+                        <div class="w-1/2">
+                            <p class="text-2xl  " dir="rtl">
+                                بدل طعام ( {{$user['allowances']['food_allowance']}} )
+                            </p></div>
+                    @endif
+
+                    @if(isset($user['allowances']['fixed_overtime']))
+                        <div class="w-1/2">
+                            <p class="text-2xl  " dir="rtl">
+                                بدل إضافي ثابت ( {{$user['allowances']['fixed_overtime']}} )
+                            </p></div>
+                    @endif
+
+                    @if(isset($user['allowances']['fixed_bonus']))
+                            <div class="w-1/2">
+                                <p class="text-2xl  " dir="rtl">
+                         بدل  ثابت  ( {{$user['allowances']['fixed_bonus']}} )
+                                </p></div>
+                    @endif
+
                 </div>
             @endif
 
@@ -214,7 +240,8 @@
             <div class="flex flex-col pt-5  px-10 ">
                 <p dir="rtl" class="text-2xl  leading-9 " style="line-height: 3rem">
                     بهذا نؤكد بأن الموظف المذكور أعلاه هو موظف لدينا وقد طلب منا تحويل راتبه الشهري إلى حسابه رقم :
-                    {{$user['iban']}} والموجود في فرعكم الرئيسي وبناء عليه نتعهد تعهداً غير مشروطاً وغير قابل
+                    {{$user['iban']}} والموجود في فرعكم الرئيسي ( بنك ساب البريطاني ) وبناء عليه نتعهد تعهداً غير
+                    مشروطاً وغير قابل
                     للنقض بأن نقوم بتحويل راتبه الشهري إلى الحساب المذكور وذلك في نهاية كل شهر ميلادي ،
                     كما نتعهد بإبلاغكم خطياً وعلى الفور في حالة فصله عن العمل أو إنتهاء خدماته معنا لأي سبب كان وذلك قبل
                     مغادرته المملكه العربيه السعوديه كما نتعهد بدفع مكافأه نهاية خدمته وتعويضاته إلى الحساب المذكور
@@ -229,15 +256,15 @@
                 </p>
                 <p class="flex text-2xl">
                     <span class="w-1/2">
-                        المسمى الوظيفي : مدير الخدمات الحكومية
+                        المسمى الوظيفي : المدير الاداري
                     </span>
                     <span class="w-1/2">
                              التوقيع:
                     </span>
                 </p>
                 <p class="flex text-2xl">
-                    <span class="w-1/2"> رقم الفاكس: {{$user['fax']}}</span>
-                    <span class="w-1/2">رقم هاتف العمل: {{$user['phone']}}</span>
+                    <span class="w-1/2"> رقم الفاكس: 0138459501</span>
+                    <span class="w-1/2">رقم هاتف العمل: 0138459502</span>
                 </p>
             </div>
 
@@ -245,7 +272,8 @@
 
             <div class="flex flex-col pt-5  px-10 ">
                 <p dir="rtl" class="text-2xl  leading-9 " style="line-height: 3rem">
-                    أفوض أنا / {{$user['ar_name']}} ، هوية رقم : {{$user['iqama_number']}} ، البنك السعودي البريطاني تفويضاً غير
+                    أفوض أنا / {{$user['ar_name']}} ، هوية رقم : {{$user['iqama_number']}} ، البنك السعودي البريطاني
+                    تفويضاً غير
                     مشروطاً وغير قابل للنقض بخصم أية مبالغ مستحقه للبنك وذلك من حسابي رقم : {{$user['iban']}} ،
                     والموجود في فرعكم الرئيسي ، كما أفوض وأتنازل للبنك السعودي البريطاني تنازلاً غير مشروطاً وغير قابل
                     للنقض عن راتبي الشهري ومستحقاتي الأخرى والمستحقه لي من وظيفتي مع {{$user['sponsor_company']}} .
@@ -254,7 +282,7 @@
 
             <div class="flex flex-col  pt-5 px-10 " dir="rtl">
                 <p class="text-2xl">
-                     التوقيع :
+                    التوقيع :
                 </p>
                 <p class="flex text-2xl">
                     <span class="w-1/2">
