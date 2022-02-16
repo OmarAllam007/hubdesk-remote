5187de56b375b42d8f21ba6e410b34ecc131517e
omarkhaled <omar.ceh007@gmail.com>
2022-02-16 14:45:35 +0300

    some updates on letters visa

---
+++ resources/views/letters/word/family/issue_visa.php
@@ -0,0 +1,71 @@
+<?php
+
+$phpWord = new \PhpOffice\PhpWord\PhpWord();
+
+
+$section = $phpWord->addSection(['marginTop' => 2400,
+    'marginLeft' => 200, 'marginRight' => 400, 'marginBottom' => 400]);
+$section->addText(
+    'HD: ' . $letterTicket->ticket->id
+);
+$section->addText('', [], []);
+$section->addText(
+    'ID: ' . $letterTicket->ticket->requester->employee_id
+);
+
+$section->addText('', [], []);
+$section->addText(
+    $letterTicket->ticket->requester->business_unit->name
+);
+
+$headerStyle = ['bold' => true, "size" => 18, 'underline' => 'single'];
+$titleStyle = ["size" => 14, 'rtl' => true];
+$rightStyle = ['align' => 'right'];
+
+
+$section->addText("التاريخ : " . $letterTicket->last_approval_date . " م", [], $rightStyle);
+$section->addText('', [], []);
+$section->addText('', [], []);
+
+
+$to = $letterTicket->ticket->fields->first() ? $letterTicket->ticket->fields->first()->value : '';
+$IstiqdamTo = $letterTicket->ticket->fields->last() ? $letterTicket->ticket->fields->last()->value : '';
+
+$regions = $letterTicket->letter->fields->where('name','like','Region')->first();
+$regions = array_combine($regions->options,$regions->options);
+
+$region_ar = \App\Translation::where('word','like',$regions[$to])
+    ->where('language','ar')->first();
+
+$to_ar = \App\Translation::where('word','like',$IstiqdamTo)
+    ->where('language','ar')->first();
+
+$section->addText('', [], []);
+$section->addText("شئون الإستقدام بـ / $to                المحترمين", $titleStyle, $rightStyle);
+$section->addText('', [], []);
+$section->addText('', [], []);
+$section->addText(' ،،، السلام عليكم ورحمة الله وبركاته ،،، وبعد', ['size' => 14], $rightStyle);
+$section->addText('', [], []);
+$section->addText('', [], []);
+$section->addText("نود إفادة سعادتكم ان موظفنا المدعو / {$user['ar_name']} ، الجنسية / {$user['ar_nationality']} والذي يعمل لدينا بمهنة / {$user['occupation']}", ['size' => 14, 'rtl' => true], $rightStyle);
+$section->addText('', [], []);
+$section->addText(" حسب المهنة الموضحة بالاقامة رقم/ {$user['iqama_number']} ، والذي يتقاضى راتب شهريا ( {$user['total_package']} ريال ) ولديه مؤهل {$user['education']} تخصص .", ['size' => 14, 'rtl' => true], $rightStyle);
+
+
+$section->addText("يرغب في استقدام / {$to_ar->translation} وحيث لا مانع لدينا من ذلك حيث ان العلاج والسكن مؤمن للمذكور ، عليه نأمل الموافقة على طلب موظفنا كما نصادق على صحة المعلومات الموضحة بخطابنا هذا ، وهذا إقرار منا بذلك .", ['size' => 14, 'rtl' => true], ['align' => 'right']);
+
+$section->addText('', [], ['spacing' => 1000]);
+$section->addText('ولكم جزيل الشكر ،،،', ['bold' => true, 'size' => 14, 'rtl' => true], ['align' => 'center']);
+$section->addText('', [], ['spacing' => 1000]);
+$section->addText($user['sponsor_company'], [], []);
+$section->addText('', [], ['spacing' => 150]);
+//$section->addText(config('letters.signature_name'), [], []);
+
+
+$objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+$temp_file = tempnam(sys_get_temp_dir(), 'PHPWord');
+
+$objWriter->save($temp_file);
+header("Content-Disposition: attachment; filename=myFile.docx");
+readfile($temp_file); // or echo file_get_contents($temp_file);
+unlink($temp_file);
