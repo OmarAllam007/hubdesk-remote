cbfab54ec574bf8f8df1e9cb4bb4b72779cd9988
omarkhaled <omar.ceh007@gmail.com>
2022-02-21 09:24:22 +0300

    ahly bank letter

--- resources/views/letters/template/salary_transfer/ahly.blade.php
+++ resources/views/letters/template/salary_transfer/ahly.blade.php
@@ -7,6 +7,7 @@
         <page size="A4" class="" style="font-size: 10pt;">
             <div class="pt-32"></div>
             <div class="pt-10 "></div>
+            <div class="pt-10 "></div>
             <div class="pt-20 px-10 "></div>
             <div class="flex px-5 ">
                 <div class="w-1/2 border border-black p-2 ">
@@ -16,37 +17,37 @@
                         <p class="pt-2 ">Subject: Salary Transfer Certificate</p>
                         <p class="pt-2 ">This is to certify that the below mentioned is our full time employee and based on authorization from him we will transfer his net monthly salary along with all other allowances and commissions (if any) regularly on a monthly basis to: and shall also deposit his final settlement including End of Service Benefits and any other allowances in case of his resignation, retirement or termination due to any reason in to his bank account at: Bank: NCB
                         </p>
-                        <p class="pt-5 ">A/C No: {{$user['iban']}}</p>
+                        <p class="pt-2  ">A/C No: {{$user['iban']}}</p>
 
-                        <p class="pt-5 ">
+                        <p class="pt-2  ">
                             IBAN NO: {{$user['iban']}}
                         </p>
 
-                        <p class="pt-5">Also the company certifies that it will not cancel this undertaking unless a written clearance from
+                        <p class="pt-2 ">Also the company certifies that it will not cancel this undertaking unless a written clearance from
                             Bank is received, moreover, in case of resignation / retirement/ termination, the company will immediately inform NCB's collections Department at Al- Khaldiya, Jeddah P.O box 19396 Jeddah 21435 or by e-mail CollectionsSkipThacing@alahli.com about the date of deposit of final settlement of this employee. In this regard, the company is not responsible for any legal or financial commitment except what is mentioned above.
                         </p>
-                        <p class="pt-5">
+                        <p class="pt-2 ">
                             Employee Name: {{$user['en_name']}}
                         </p>
-                        <p class="pt-5">
+                        <p class="pt-2 ">
                             ID NO: {{$user['iqama_number']}}
                         </p>
-                        <p class="pt-5">
+                        <p class="pt-2 ">
                             Hire Date: {{$user['date_of_join']}}
                         </p>
 
-                        <p class="pt-5">
+                        <p class="pt-2 ">
                             Job description: {{$user['en_occupation']}}
                         </p>
 
-                        <p class="pt-5">
+                        <p class="pt-2 ">
                             Net monthly Salary: {{$user['total_package']}} SR
                         </p>
-                        <p class="pt-5">
+                        <p class="pt-2 ">
                             Salary Deposit Date: First day of every month.
                         </p>
 
-                        <p class="text-center pt-5  ">
+                        <p class="text-center pt-2   ">
                             {{\App\Helpers\LetterSponserMap::$en_sponsers[$user['sponsor_id']]}}
                         </p>
                     </div>
@@ -56,50 +57,50 @@
                     <p class="pt-2  ">السادة: بنك الاهلي السعودي</p>
                     <p class="pt-2  ">الموضوع: شهادة تحويل راتب</p>
                     <p class="pt-2  ">نشهد بان المذكور ادناه يعمل لديها بنظام التفرغ الكامل وبناء على تفويض منه فسوف يتم تحويل صافي راتبه مع كامل الدبلات والعمولات الشهرية (إن وجدت) وجميع مستحقاته النهائية( شاملة مكافاة نهاية الخدمة واي بدلات بعد الاستقالة) في حالة الاستقالة او الفصل او التقاعد (لأي سبب كانت الاستقالة او الفصل او التقاعد)</p>
-                    <p class="pt-5 ">الى حسابه المصرفي لدى:
+                    <p class="pt-2  ">الى حسابه المصرفي لدى:
                         البنك الاهلي التجاري
                     </p>
                     @php
                         $IbanField = $letterTicket->ticket->fields->first() ? $letterTicket->ticket->fields->first()->value : '';
                     @endphp
-                    <p class="pt-5 ">
+                    <p class="pt-2  ">
                         رقم الحساب: {{$user['iban']}}
                     </p>
 
 
-                    <p class="pt-5 ">
+                    <p class="pt-2  ">
                         رقم الحساب الدولي: {{$user['iban']}}
                     </p>
-                    <p class="pt-5">
+                    <p class="pt-2 ">
                          كما تلتزم الشركة بعدم السماح للموظف بالغاء او تعديل التفويض الا بموافقة البنك الخطية، هذا بالاضافة انه في حالة استقالة الموظف او فصله او تقاعده فان الشركة تلتزم باخطار البنك الاهلي السعودي (ادارة التحصيل بحي الخالدية بجدة) او بواسطة البريد الالكتروني CollectionsSkipThacing@alahli.com
                         بتاريخ ايداع مستحقاته النهائية.
                     </p>
 
-                    <p class="pt-5">
+                    <p class="pt-2 ">
                         كما ان الشركة لا تتحمل اي التزام قانوني او مالي بخلاف ما ذكر اعلاه
                     </p>
-                    <p class="pt-5">
+                    <p class="pt-2 ">
                         اسم الموظف: {{$user['ar_name']}}
                     </p>
-                    <p class="pt-5">
+                    <p class="pt-2 ">
                         رقم الهوية: {{$user['iqama_number']}}
                     </p>
 
-                    <p class="pt-5">
+                    <p class="pt-2 ">
                         تاريخ التعيين: {{$user['date_of_join']}}
                     </p>
 
-                    <p class="pt-5">
+                    <p class="pt-2 ">
                         المسمى الوظيفي: {{$user['occupation']}}
                     </p>
 
-                    <p class="pt-5">
+                    <p class="pt-2 ">
                         صافي الراتب الشهري: {{$user['total_package']}} ريال
                     </p>
-                    <p class="pt-5">
+                    <p class="pt-2 ">
                         تاريخ ايداعه: في اليوم الاول من كل شهر
                     </p>
-                    <p class="text-center pt-5   ">
+                    <p class="text-center pt-2    ">
                         {{$user['sponsor_company']}}
                     </p>
                 </div>
