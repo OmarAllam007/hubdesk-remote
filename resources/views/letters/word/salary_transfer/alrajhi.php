9a3588aa95e476af7ff95eac96d9fa544502b471
omarkhaled <omar.ceh007@gmail.com>
2022-02-24 15:03:05 +0300

    al-rajhi bank word

---
+++ resources/views/letters/word/salary_transfer/alrajhi.php
@@ -0,0 +1,130 @@
+<?php
+
+$phpWord = new \PhpOffice\PhpWord\PhpWord();
+
+
+$IbanField = $letterTicket->ticket->fields->first() ? $letterTicket->ticket->fields->first()->value : '';
+
+
+$titleStyle = ["size" => 14, 'rtl' => true];
+$rightStyle = ['align' => 'right'];
+
+$section = $phpWord->addSection(['marginTop' => 2400,
+    'marginLeft' => 200, 'marginRight' => 400, 'marginBottom' => 400]);
+
+$section->addText(
+    'الموافق: ' . $letterTicket->last_approval_date, [], $rightStyle);
+$section->addText('', [], []);
+
+
+$section->addText("المكرمون / {$letterTicket->letter->ar_name}  ", [], $rightStyle);
+
+$section->addText('', [], []);
+$section->addText(' ،،، السلام عليكم ورحمة الله وبركاته ،،، وبعد', [], $rightStyle);
+$section->addText('', [], []);
+$section->addText('اسم الموظف: ' . $user['ar_name'], [], $rightStyle);
+$section->addText('', [], []);
+$section->addText('رقم بطاقة العمل/الرقم الوظیفي: ' . $user['employee_id'], [], $rightStyle);
+$section->addText('', [], []);
+$section->addText('رقم إثبات الهویة: ' . $user['iqama_number'], [], $rightStyle);
+$section->addText('', [], []);
+$section->addText('مسـمى الـوظیـفة: ' . $user['occupation'], [], $rightStyle);
+$section->addText('', [], []);
+$section->addText('تاریخ الالتحاق بالعمل: ' . $user['date_of_join'], [], $rightStyle);
+$section->addText('', [], []);
+$longStr = "تقدم موظفنا الموضحة بیاناته أعلاه طالباً تحویل راتبه وبدلاته الشهریة وجمیع مستحقاته الوظیفیة إلى حسابه الجاري رقم";
+$longStr .= '(' . $user['iban'] . ')';
+$longStr .= 'لأجل سداد الدیون التي سوف تترتب علیه لصالح مصرف الراجحي. لذا نود أن نؤكد لكم موافقتنا والتزامنا بهذا 
+التحویل في مواعیده الشهریة واستمراره حتى نهایة علاقته الوظیفیة معنا مع التزامنا بعدم تسلیم الموظف المذكور مستحقات نهایة الخدمة أو أي مستحقات
+أخرى حتى نحصل على مخالصة معتمدة منكم تفید بانتهاء الالتزامات المترتبة علیه لصالحكم وموافقتكم على إلغاء تحویل راتبه. وتقبلوا تحیاتي.';
+$section->addText('', [], []);
+$section->addText($longStr, ['rtl' => true], $rightStyle);
+
+
+$section->addText("", ['size' => 12], $rightStyle);
+
+
+$section->addText('', [], ['spacing' => 1000]);
+$section->addText($user['sponsor_company'], [], []);
+$section->addText('', [], ['spacing' => 150]);
+$section->addText(config('letters.signature_name'), [], []);
+//
+$section->addPageBreak();
+
+
+$section = $phpWord->addSection(['marginTop' => 2400,
+    'marginLeft' => 200, 'marginRight' => 400, 'marginBottom' => 400]);
+
+
+$headerStyle = ['bold' => true, 'underline' => 'single', 'rtl' => 'right'];
+$titleStyle = [ 'rtl' => true];
+$rightStyle = ['align' => 'right'];
+
+
+/** @var \App\LetterTicket $letterTicket */
+
+$section->addText(" التاريخ : " . $letterTicket->last_approval_date . " م", [], $rightStyle);
+$section->addText('', [], []);
+$section->addText('', [], []);
+$section->addText('الموضوع : تعريف بالراتب', $headerStyle,
+    ['align' => 'right']);
+$section->addText('', [], []);
+$section->addText("الســادة / {$letterTicket->letter->ar_name}                        المحترمين", $titleStyle, $rightStyle);
+$section->addText('', [], []);
+$section->addText(' السلام عليكم ورحمة الله وبركاته ', [], $rightStyle);
+$section->addText('', [], []);
+
+$tableStyle = array(
+    'borderColor' => 'fff',
+    'borderSize' => 6,
+    'cellMargin' => 50,
+    'unit' => \PhpOffice\PhpWord\SimpleType\TblWidth::PERCENT,
+    'width' => 100 * 50,
+    'alignment' => \PhpOffice\PhpWord\SimpleType\JcTable::CENTER
+);
+$firstRowStyle = array('bgColor' => 'faf3ed');
+$phpWord->addTableStyle('myTable', $tableStyle, $firstRowStyle);
+$table = $section->addTable('myTable');
+
+$cellColor = 'f2f2f2';
+addNewRow($table, 'اسم الموظف', 'الجنسية', $cellColor);
+addNewRow($table, $user['ar_name'], $user['ar_nationality']);
+addNewRow($table, 'رقم إثبات الهوية', 'الرقم الوظيفي', $cellColor);
+addNewRow($table, $user['iqama_number'], $letterTicket->ticket->requester->employee_id);
+addNewRow($table, 'الوظيفة', 'تاريخ التعيين', $cellColor);
+addNewRow($table, $user['occupation'], $user['date_of_join']);
+addNewRow($table, 'الراتب الأساسي', 'إجمالي الراتب', $cellColor);
+addNewRow($table, $user['allowances']['basic_salary'], $user['total_package']);
+
+$table->addRow()->addCell(100 * 50, ['gridSpan' => 2, 'bgColor' => $cellColor])
+    ->addText('تفاصيل البدلات', [], ['align' => 'right',]);
+
+$table->addRow()->addCell(100 * 50, ['gridSpan' => 2])
+    ->addText("{$user['allowances_str']}", [], ['align' => 'right', 'bidi' => false]);
+
+$section->addText('', [], []);
+$section->addText('', [], []);
+$section->addText('كما نفيدكم أن الموظف المذكور بياناته أعلاه لازال يعمل لدينا حتى تاريخه،
+ وقد اعطي هذا الخطاب بناء على طلبه دون ادنى مسؤولية على الشركة.', ['rtl' => true,
+    'bold' => true, 'size' => 12], ['align' => 'right']);
+
+$section->addText('', [], ['spacing' => 1000]);
+$section->addText($user['sponsor_company'], ['rtl' => true], []);
+$section->addText('', [], ['spacing' => 150]);
+$section->addText(config('letters.signature_name'), ['rtl' => true], []);
+
+/**
+ * @param \PhpOffice\PhpWord\Element\Table $table
+ * @param $text1
+ * @param $text2
+ */
+function addNewRow($table, $text1, $text2, $color = 'fff', $gridSpan = 2)
+{
+    $row1 = $table->addRow();
+    $row1->addCell(100 * 50, ['bgColor' => $color, 'vMerge' => 'restart'])
+        ->addText($text1, [], ['align' => 'right']);
+    $row1->addCell(100 * 50, ['bgColor' => $color, 'vMerge' => 'restart'])
+        ->addText($text2, [], ['align' => 'right']);
+}
+
+require resource_path() . '/views/letters/word/_footer.php';
