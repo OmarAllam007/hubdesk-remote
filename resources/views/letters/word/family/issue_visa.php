2448c962d04fcd95b06c00f3568d81328a39b8c5
omarkhaled <omar.ceh007@gmail.com>
2022-04-05 11:34:04 +0300

    issue visa issue

--- resources/views/letters/word/family/issue_visa.php
+++ resources/views/letters/word/family/issue_visa.php
@@ -12,8 +12,8 @@ $region_ar = \App\Translation::where('word','like',$regions[$to])
 $to_ar = \App\Translation::where('word','like',$IstiqdamTo)
     ->where('language','ar')->first();
 
-$education = $letterTicket->ticket->fields->where('name','Academic Qualification')->first();
-$specialization = $letterTicket->ticket->fields->where('name','Specialization')->first();
+$education = $letterTicket->ticket->fields->where('name','Academic Qualification')->first() ?? '';
+$specialization = $letterTicket->ticket->fields->where('name','Specialization')->first() ?? '';
 
 $phpWord = new \PhpOffice\PhpWord\PhpWord();
 
@@ -52,8 +52,7 @@ $section->addText('', [], []);
 $section->addText('', [], []);
 $section->addText("نود إفادة سعادتكم ان موظفنا المدعو / {$user['ar_name']} ، الجنسية / {$user['ar_nationality']} والذي يعمل لدينا بمهنة / {$user['occupation']}", ['size' => 14, 'rtl' => true], $rightStyle);
 $section->addText('', [], []);
-
-$section->addText(" حسب المهنة الموضحة بالاقامة رقم/ {$user['iqama_number']} ، والذي يتقاضى راتب شهريا ( {$user['total_package']} ريال ) ولديه مؤهل {$education}  تخصص {$specialization} .", ['size' => 14, 'rtl' => true], $rightStyle);
+$section->addText(" حسب المهنة الموضحة بالاقامة رقم/ {$user['iqama_number']} ، والذي يتقاضى راتب شهريا ( {$user['total_package']} ريال ) ولديه مؤهل {$education->value}  تخصص {$specialization->value} .", ['size' => 14, 'rtl' => true], $rightStyle);
 
 
 $section->addText("يرغب في استقدام / {$to_ar->translation} وحيث لا مانع لدينا من ذلك حيث ان العلاج والسكن مؤمن للمذكور ، عليه نأمل الموافقة على طلب موظفنا كما نصادق على صحة المعلومات الموضحة بخطابنا هذا ، وهذا إقرار منا بذلك .", ['size' => 14, 'rtl' => true], ['align' => 'right']);
