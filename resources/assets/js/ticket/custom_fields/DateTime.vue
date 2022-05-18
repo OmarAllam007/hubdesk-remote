a89689663c23292c0ca4fbdb5d83db12087f2f6b
omarkhaled <omar.ceh007@gmail.com>
2022-05-18 08:49:03 +0300

    wip

---
+++ resources/assets/js/ticket/custom_fields/DateTime.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="flex flex-col ">
+      <label :for="id"
+             class="font-semibold">
+        {{ $root.t(label) }} <span v-if="required == '1'" class="text-red-700 font-bold">*</span>
+      </label>
+      <input
+          type="datetime-local"
+          :id="id"
+          class=" px-4 py-2
+        transition duration-300 border border-gray-400 rounded select2"
+          :value="value"
+          @input="$emit('input', {id:item_id,value:$event.target.value})"
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DateTime",
+  props: ['id', 'name', 'label', 'value','required','item_id']
+}
+</script>
+
+<style scoped>
+
+</style>
