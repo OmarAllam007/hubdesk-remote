1ec72dd06863f82c17dec7ca22d130c4e97831f9
omargarana <@Matador99>
2022-10-02 09:52:27 +0300

    wip

--- resources/views/e-card/admin/user/index.blade.php
+++ resources/views/e-card/admin/user/index.blade.php
@@ -21,8 +21,8 @@
 @section('body')
     <div class="flex w-full p-5 ">
         @if ($users->total())
-            <section class="bg-white shadow-md rounded my-5 w-full">
-                <table class="min-w-max w-full table-auto">
+            <div class="bg-white shadow-md rounded my-5 w-full overflow-x-scroll">
+                <table class="min-w-max w-full table-auto ">
                     <thead class="bg-gray-300 text-gray-800  uppercase  leading-normal">
                     <tr>
                         <th class="py-5  px-6 text-left">{{t('Name')}}</th>
@@ -35,7 +35,7 @@
                         <th class="py-5  px-6 text-left">{{t('Actions')}}</th>
                     </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="overflow-x-scroll">
                     @foreach($users as $user)
                         <tr class="border-b border-gray-200 hover:bg-yellow-100">
                             <td class="py-5  px-6 text-left whitespace-nowrap"><a
@@ -61,9 +61,9 @@
                             <td class="py-5  px-6 text-left whitespace-nowrap">
                                 <form action="{{ route('e-card.admin.user.destroy', $user) }}" method="post">
                                     {{csrf_field()}} {{method_field('delete')}}
-                                    <a class="btn btn-sm btn-primary" href="{{ route('e-card.admin.edit', $user) }} "><i
-                                                class="fa fa-edit"></i> Edit</a>
-                                    <button class="btn btn-sm btn-warning"><i class="fa fa-remove"></i> Delete</button>
+                                    <a class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" href="{{ route('e-card.admin.edit', $user) }} "><i
+                                                class="fa fa-edit"></i></a>
+                                    <button class="bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"><i class="fa fa-remove"></i></button>
                                 </form>
                             </td>
                         </tr>
@@ -72,7 +72,7 @@
                 </table>
 
                 @include('partials._pagination', ['items' => $users])
-            </section>
+            </div>
         @else
             <div class="w-full p-5">
                 <div class="p-5 bg-blue-300 shadow rounded-xl"><i class="fa fa-exclamation-circle"></i> <strong>No users found</strong></div>
