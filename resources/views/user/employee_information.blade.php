c7a9c1095a3b17ca944d58488276ce27bdd5e869
Omar Garana <omar.garana@hubtech.sa>
2022-11-17 13:35:55 +0300

    move to production

--- resources/views/user/employee_information.blade.php
+++ resources/views/user/employee_information.blade.php
@@ -12,21 +12,13 @@
     <div class="flex">
         <div class="w-full">
             <div class="flex-col justify-center items-center">
-{{--                <div class="flex  pb-5 justify-center">--}}
-{{--                    @foreach($months as $key=>$month)--}}
-{{--                        <a class="m-3 w-1/6 text-center py-5  rounded-xl shadow-md  bg-white font-bold--}}
-{{--                  @if(request('index') == $key+1) bg-gray-300 shadow-sm @endif"--}}
-{{--                           href="{{route('user.information')}}?index={{$key+1}}"--}}
 
-{{--                        >--}}
-{{--                            {{$month->format('M')}}</a>--}}
-{{--                    @endforeach--}}
-{{--                </div>--}}
                 <div class="flex justify-center">
                     <div class="w-10"></div>
                     <div class="flex flex-col justify-center p-10 ">
                         @foreach($paths as $path)
-                            <iframe src="{{URL::to($path)}}"  height='800' allowfullscreen="" class="pt-5 "></iframe>
+
+                            <iframe src="./{{$path}}"  height='800' allowfullscreen="" class="pt-5 "></iframe>
                             {{--                        <iframe id="salarySlip" class="w-full pt-5 "--}}
                             {{--                                src="{{route('user.salarySlipPdf')}}?index={{request('index',0)}}" scrolling="auto"--}}
                             {{--                                height="1100"--}}
