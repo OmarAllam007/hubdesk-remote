16ec7484ee2b7ede3bf8b77eaf99b82ac5aa2a55
omargarana <@Matador99>
2022-12-21 10:54:26 +0300

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HubDesk</title>
-    {{--    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">--}}
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}">
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Mada:400,700">
     {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.css">--}}
