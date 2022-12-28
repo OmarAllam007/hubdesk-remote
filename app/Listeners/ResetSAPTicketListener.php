49e3872386322a63a1e99ec5222f8936e44150bf
omargarana <@Matador99>
2022-12-28 12:04:12 +0300

    change the SAP Client ssl configurations

--- app/Listeners/ResetSAPTicketListener.php
+++ app/Listeners/ResetSAPTicketListener.php
@@ -6,6 +6,7 @@ use App\Jobs\TicketReplyJob;
 use App\Mail\ReplyTicketMail;
 use App\Mail\SAPResetPasswordEmail;
 use App\TicketReply;
+use App\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Laminas\Soap\Client;
@@ -17,24 +18,66 @@ class ResetSAPTicketListener
     const NOT_EXIST = "User does not exist";
 
     private $user;
-
+    private $client;
     public static $SELECTION_MAP_URLS;
 
     public function __construct()
     {
-        $this->user = auth()->user();
+        $this->user = User::where('employee_id', 90000970)->first();
 
         self::$SELECTION_MAP_URLS = [
             'SAP ECC Quality 400 (ECC QAS 400)' => config('sap.ecc.qas400'),
             'SAP ECC Quality 500 (ECC QAS 500)' => config('sap.ecc.qas500'),
             'SAP ECC Quality 920 (ECC QAS 920)' => config('sap.ecc.qas920'),
             'SAP ECC Production 900 (ECC PRD 900)' => config('sap.ecc.prd'),
-            'SAP S4Hana Quality 900 (S4Hana QAS 220)' => config('sap.s4hana.qas'),
+            'SAP S4Hana Quality 220 (S4Hana QAS 220)' => config('sap.s4hana.qas'),
             'SAP S4Hana Production 300 (S4Hana PRD 300)' => config('sap.s4hana.prd'),
             'SolMan' => config('sap.solman'),
         ];
     }
 
+    private function initClient($serverObject)
+    {
+        $this->client = new Client();
+        $this->client->setUri($serverObject['url']);
+        $this->client->setOptions([
+            'soap_version' => SOAP_1_2,
+            'wsdl' => $serverObject['url'],
+            'login' => $serverObject['user'],
+            'password' => $serverObject['password'],
+            'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP,
+            'streamcontext' => stream_context_create([
+                'ssl' => [
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true,
+
+                ],
+                'http' => array(
+                    "timeout" => 60,
+                ),
+            ])
+        ]);
+    }
+
+    private function tryResetPassword($ticket)
+    {
+        try{
+            $employeeID = "E{$this->user->employee_id}";
+            $result = $this->client->ZHUBDESK_CHANGE_USER_PASSWORD(['IM_BNAME' => $employeeID]);
+
+            if (strtolower($result->EX_MESSAGE) == strtolower(self::SUCCESS_MESSAGE)) {
+                $this->createReply($ticket, $result, true);
+            } else {
+                $this->createReply($ticket, $result, false);
+            }
+        }catch (\Throwable $e) {
+            if($e->getMessage() == "Could not connect to host"){
+                $this->tryResetPassword($ticket);
+            }
+        }
+
+    }
 
     public function handle($ticket)
     {
@@ -52,39 +95,9 @@ class ResetSAPTicketListener
 
                 if (!$serverObject['url']) return;
 
-                $client = new Client();
-                $client->setUri($serverObject['url']);
-                $client->setOptions([
-                    'soap_version' => SOAP_1_2,
-                    'wsdl' => $serverObject['url'],
-                    'login' => $serverObject['user'],
-                    'password' => $serverObject['password'],
-                    'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP,
-                    'streamcontext'=> stream_context_create([
-                        'ssl' => [
-                            'verify_peer' => false,
-                            'verify_peer_name' => false,
-                            'allow_self_signed' => true
-                        ],
-                        'http' => array(
-                            "timeout" => 60,
-                        ),
-                    ])
-                ]);
+                $this->initClient($serverObject);
+                $this->tryResetPassword($ticket);
 
-                try {
-                    $employeeID = "E{$this->user->employee_id}";
-                    $result = $client->ZHUBDESK_CHANGE_USER_PASSWORD(['IM_BNAME' => $employeeID]);
-
-                    if (strtolower($result->EX_MESSAGE) == strtolower(self::SUCCESS_MESSAGE)) {
-                        $this->createReply($ticket, $result, true);
-                    } else {
-                        $this->createReply($ticket, $result, false);
-                    }
-                } catch (\Throwable $e) {
-                    $this->createReply($ticket, null, false);
-                    return $e->getMessage();
-                }
             } else {
                 // @TODO: send email with invalid user information
                 $this->createReply($ticket, null, false);
@@ -122,7 +135,7 @@ class ResetSAPTicketListener
     private function makeContent($isValidUser, $result)
     {
         $resultStr = "";
-        if ($isValidUser && $result["EX_MESSAGE"] == self::SUCCESS_MESSAGE) {
+        if ($isValidUser && $result->EX_MESSAGE == self::SUCCESS_MESSAGE) {
             $resultStr .= "<p>Username: e{$this->user->employee_id}</p>";
             $resultStr .= "<p>Password: {$result->EX_PASSWORD}</p>";
         } else {
