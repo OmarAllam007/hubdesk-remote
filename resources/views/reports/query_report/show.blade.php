02a357351bb82178840e92d2c7cc0437a80500bd
Omar Garana <omar.garana@hubtech.sa>
2023-01-11 16:34:40 +0300

    reports issue

--- resources/views/reports/query_report/show.blade.php
+++ resources/views/reports/query_report/show.blade.php
@@ -25,35 +25,38 @@
     @endif
 
     @if(collect($columns)->count())
-        <div class="container-fluid report-container">
-            <section class="">
-                <table class="table table-condensed report-head">
-                    <thead>
-                    <tr>
-                        @foreach($columns as $column)
-                            <th>{{ t($column) }}</th>
-                        @endforeach
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($data as $item)
+        <div class="p-3">
+            <div class="flex w-full shadow-sm">
+                <section class="overflow-x-auto rounded-md p-2 ">
+                    <table class="table-fixed">
+                        <thead class="bg-gray-200">
                         <tr>
-                            @foreach($columns as $key=>$column)
-                                <td>{{str_replace('&nbsp;','',strip_tags(collect($item)->toArray()[$column])) ?? '' }}</td>
-                                {{--                                <td>{!! html_entity_decode(collect($item)->toArray()[$column] )?? ''   !!}</td>--}}
+                            @foreach($columns as $column)
+                                <th
+                                        class="w-1/2 px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center  font-semibold text-gray-600 uppercase tracking-wider">{{ t($column) }}</th>
                             @endforeach
                         </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </section>
-
-
-            <div class="text-center">
-                {{ $data->appends($_GET)->links() }}
+                        </thead>
+                        <tbody>
+                        @foreach($data as $item)
+                            <tr class="bg-white hover:bg-yellow-100">
+                                @foreach($columns as $key=>$column)
+                                    <td class="text-gray-700 px-5 py-3 border-b-2 border-gray-200 text-center text-md text-black  tracking-wider">{{str_replace('&nbsp;','',strip_tags(collect($item)->toArray()[$column])) ?? '' }}</td>
+                                    {{--                                <td>{!! html_entity_decode(collect($item)->toArray()[$column] )?? ''   !!}</td>--}}
+                                @endforeach
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </section>
             </div>
-
         </div>
+
+
+        <div class="text-center">
+            {{ $data->appends($_GET)->links() }}
+        </div>
+
     @endif
 @endsection
 
